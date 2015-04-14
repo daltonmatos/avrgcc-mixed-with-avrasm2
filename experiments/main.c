a5398836c313cf3c86d9bc0f3e8c1afa7807db52
Dalton Barreto <daltonmatos@gmail.com>
2015-04-14 15:23:01 -0300

    Base C file to link against each asm example

--- experiments/main.c
+++ experiments/main.c
@@ -4,17 +4,25 @@
 // Arduino Pin13 is mapped to PORTB, bit 5
 // See: http://www.arduino.cc/en/Reference/PortManipulation
 
+//extern char _binary_blinks_bin_start(char n);
+extern char ASM_SYM(char n);
 
 int main(void){
 
+  uint8_t total_blinks =  ASM_SYM(0);
   DDRB |= _BV(PB5); /* PIN13 (internal led) as output*/
 
+  PORTB |= _BV(PB5); /* HIGH */
   for (;;){
-    PORTB |= _BV(PB5); /* HIGH */
-    _delay_ms(500);
-    
-    PORTB &= ~_BV(PB5); /* LOW */
-    _delay_ms(500);
+    uint8_t i;
+    for (i = 0; i < total_blinks; i++){
+      PORTB |= _BV(PB5); /* HIGH */
+      _delay_ms(200);
+      
+      PORTB &= ~_BV(PB5); /* LOW */
+        _delay_ms(200);
+    }
+    _delay_ms(1000);
   }
 
   return 0;
