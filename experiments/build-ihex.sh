e0ec0612eb73ba5117bfadc392b48be327e2b0b4
Dalton Barreto <daltonmatos@gmail.com>
2015-04-22 00:52:40 -0300

    Fixed symbol name to _blinks and adjusted symbol table in the binary

--- experiments/build-ihex.sh
+++ experiments/build-ihex.sh
@@ -2,7 +2,8 @@
 source ~/.zshrc
 
 ASM_SRC=${1}
-SYMBOL_NAME="_binary_build_${ASM_SRC:r}_asm_bin_start"
+#SYMBOL_NAME="_binary_build_${ASM_SRC:r}_asm_bin_start"
+SYMBOL_NAME="_blinks"
 
 
 echo ${ASM_SRC}
@@ -13,8 +14,13 @@ avr-objcopy -j .sec1 -I ihex -O binary build/${ASM_SRC}.hex build/${ASM_SRC}.bin
 #avr-objcopy --rename-section .data=.progmem.data,contents,alloc,load,readonly,data  -I binary -O elf32-avr build/${ASM_SRC}.bin build/${ASM_SRC}.elf
 avr-objcopy --rename-section .data=.text,contents,alloc,load,readonly,code -I binary -O elf32-avr build/${ASM_SRC}.bin build/${ASM_SRC}.elf
 
+cd build
+../tools/elf-add-symbol ${ASM_SRC}.elf
+cd -
 
-avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/${ASM_SRC}.elf
+
+#avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/${ASM_SRC}.elf
+avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/with_symbols_${ASM_SRC}.elf
 avr-objcopy -I elf32-avr -O ihex -j .text -j .data build/main_${ASM_SRC}.elf build/main_${ASM_SRC}.hex
 
 sudo /usr/share/arduino/hardware/tools/avrdude -C /usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -cusbasp -Uflash:w:build/main_${ASM_SRC}.hex:i
