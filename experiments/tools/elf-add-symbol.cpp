fdb25aa1e29dad8f7a9a6a3187ebc41dea5e54e8
Dalton Barreto <daltonmatos@gmail.com>
2015-04-22 00:32:26 -0300

    Working example with relocation table.
    Relocation table:
    
    OFFSET means witch instruction in the original
    code must have its call address recalculated
    VALUE means "to where" this address must point to in the final binary
    
    18 is Relocation type R_AVR_CALL

--- experiments/tools/elf-add-symbol.cpp
+++ experiments/tools/elf-add-symbol.cpp
@@ -1,5 +1,4 @@
 #include <elfio/elfio.hpp>
-#include <stdio.h>
 
 
 using namespace ELFIO;
@@ -8,7 +7,7 @@ int main(int argc, char** argv){
   elfio elf_src;
 
   elf_src.load(argv[1]);
-
+  std::string output_file(argv[1]);
 
   section *text_sec, *strtab_sec, *shstrtab_sec, *symtab_sec;
 
@@ -55,12 +54,36 @@ int main(int argc, char** argv){
   symbol_section_accessor syma(elf_src, symtab_sec);
   string_section_accessor stra(strtab_sec);
 
-  syma.add_symbol(stra, "_add_10", 0x6, 0, STB_GLOBAL, STT_NOTYPE, 0, text_sec->get_index()); 
-  syma.add_symbol(stra, "_main", 0x0, 0, STB_GLOBAL, STT_NOTYPE, 0, text_sec->get_index()); 
+  syma.add_symbol(stra, "_blinks", 0x0, 9, STB_GLOBAL, STT_NOTYPE, 0, text_sec->get_index()); 
+  Elf_Word _real_code_to_adjust = syma.add_symbol(stra, "_real_code", 0xa, 0, STB_GLOBAL, STT_NOTYPE, 0, text_sec->get_index()); 
+  Elf_Word _clear_to_adjust = syma.add_symbol(stra, "_clear", 0x10, 0, STB_GLOBAL, STT_NOTYPE, 0, text_sec->get_index()); 
+
+  
+    // Create relocation table section
+    section* rel_sec = elf_src.sections.add( ".rel.text" );
+    rel_sec->set_type      ( SHT_REL );
+    rel_sec->set_info      ( text_sec->get_index() );
+    rel_sec->set_addr_align( 0x4 );
+    rel_sec->set_entry_size( elf_src.get_default_entry_size( SHT_REL ) );
+    rel_sec->set_link      ( symtab_sec->get_index() );
+
+    // Create relocation table writer
+    relocation_section_accessor rela( elf_src, rel_sec );
+
+    /* 18 is R_AVR_CALL (relocatin type) */
+    rela.add_entry( 0x4, _real_code_to_adjust, (unsigned char) 18 );
+    rela.add_entry( 0x0, _clear_to_adjust, (unsigned char) 18 );
+
+    // Another method to add the same relocation entry at one step is:
+    // rela.add_entry( stra, "msg",
+    //                 syma, 29, 0,
+    //                 ELF_ST_INFO( STB_GLOBAL, STT_OBJECT ), 0,
+    //                 text_sec->get_index(),
+    //                 place_to_adjust, (unsigned char)R_386_RELATIVE );
 
 
   std::cout << "Saving elf." << std::endl;  
-  elf_src.save("saved.elf");
+  elf_src.save("with_symbols_" + output_file.substr(output_file.find_last_of("/")+1));
  
   return 0;
 }
