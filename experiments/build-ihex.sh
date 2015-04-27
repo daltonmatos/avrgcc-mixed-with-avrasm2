510ec3325464bb7ef1f066c24e65e826afee5132
Dalton Barreto <daltonmatos@gmail.com>
2015-04-26 21:08:49 -0300

    Update script to generate final elf

--- experiments/build-ihex.sh
+++ experiments/build-ihex.sh
@@ -15,16 +15,18 @@ avr-objcopy -j .sec1 -I ihex -O binary build/${ASM_SRC}.hex build/${ASM_SRC}.bin
 avr-objcopy --rename-section .data=.text,contents,alloc,load,readonly,code -I binary -O elf32-avr build/${ASM_SRC}.bin build/${ASM_SRC}.elf
 
 
+avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_start build/${ASM_SRC}.elf
+avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_end build/${ASM_SRC}.elf
+avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_size build/${ASM_SRC}.elf
+
 #Generate output to reconstruct the symbol table and relocation table
 avr-objdump -d build/${ASM_SRC}.elf| python2 tools/extract-symbols-metadata.py build/${ASM_SRC}.map > build/${ASM_SRC}.symtab
 
 #cd build
-#../tools/elf-add-symbol ${ASM_SRC}.elf
+cat build/${ASM_SRC}.symtab | tools/elf-add-symbol build/${ASM_SRC}.elf
 #cd -
 
-
-#avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/${ASM_SRC}.elf
-avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/with_symbols_${ASM_SRC}.elf
+avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/${ASM_SRC}.elf
 avr-objcopy -I elf32-avr -O ihex -j .text -j .data build/main_${ASM_SRC}.elf build/main_${ASM_SRC}.hex
 
 sudo /usr/share/arduino/hardware/tools/avrdude -C /usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -cusbasp -Uflash:w:build/main_${ASM_SRC}.hex:i
