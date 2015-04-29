605fc4516f270ac583841be192836de847fbdeb6
Dalton Barreto <daltonmatos@gmail.com>
2015-04-29 17:45:55 -0300

    Generic build script
    Useful to build other examples

--- experiments/build-ihex.sh
+++ experiments/build-ihex.sh
@@ -2,37 +2,48 @@
 source ~/.zshrc
 RELOC=1
 
+AVR_CHIP="atmega644p"
+
+
 ASM_SRC=${1}
-SYMBOL_NAME="_binary_build_${ASM_SRC:r}_asm_bin_start"
+ASM_FILE=`basename ${1}`
+BUILD_DIR=`dirname ${ASM_SRC}`/build
+
+SYMBOL_PREFIX="_binary_`echo ${BUILD_DIR}/${ASM_FILE} | tr / _ | tr - _ | tr . _`_bin"
+SYMBOL_NAME="${SYMBOL_PREFIX}_start"
+
 
 if [ ${RELOC} -eq 1 ]; then
   SYMBOL_NAME="_blinks"
 fi
 
+mkdir -p `dirname ${ASM_SRC}`/build
 
 
-echo ${ASM_SRC}
-echo ${SYMBOL_NAME}
-wine ~/bin/AvrAssembler2/avrasm2.exe ${ASM_SRC} -fI -o build/${ASM_SRC}.hex -l build/${ASM_SRC}.lst -m build/${ASM_SRC}.map
-avr-objcopy -j .sec1 -I ihex -O binary build/${ASM_SRC}.hex build/${ASM_SRC}.bin
+echo "building" ${ASM_SRC}
+echo "Using symbolname="${SYMBOL_NAME}
+echo "symbol prefix="${SYMBOL_PREFIX}
+
+wine ~/bin/AvrAssembler2/avrasm2.exe ${ASM_SRC} -fI -o ${BUILD_DIR}/${ASM_FILE}.hex -l ${BUILD_DIR}/${ASM_FILE}.lst -m ${BUILD_DIR}/${ASM_FILE}.map
+avr-objcopy -j .sec1 -I ihex -O binary ${BUILD_DIR}/${ASM_FILE}.hex ${BUILD_DIR}/${ASM_FILE}.bin
 
 #avr-objcopy --rename-section .data=.progmem.data,contents,alloc,load,readonly,data  -I binary -O elf32-avr build/${ASM_SRC}.bin build/${ASM_SRC}.elf
-avr-objcopy --rename-section .data=.text,contents,alloc,load,readonly,code -I binary -O elf32-avr build/${ASM_SRC}.bin build/${ASM_SRC}.elf
+avr-objcopy --rename-section .data=.text,contents,alloc,load,readonly,code -I binary -O elf32-avr ${BUILD_DIR}/${ASM_FILE}.bin ${BUILD_DIR}/${ASM_FILE}.elf
 
 if [ ${RELOC} -eq 1 ]; then
-  avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_start build/${ASM_SRC}.elf
-  avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_end build/${ASM_SRC}.elf
-  avr-strip -N _binary_build_${ASM_SRC:r}_asm_bin_size build/${ASM_SRC}.elf
+  avr-strip -N ${SYMBOL_PREFIX}_start ${BUILD_DIR}/${ASM_FILE}.elf
+  avr-strip -N ${SYMBOL_PREFIX}_end ${BUILD_DIR}/${ASM_FILE}.elf
+  avr-strip -N ${SYMBOL_PREFIX}_size ${BUILD_DIR}/${ASM_FILE}.elf
 fi
 
 #Generate output to reconstruct the symbol table and relocation table
-avr-objdump -d build/${ASM_SRC}.elf| python2 tools/extract-symbols-metadata.py build/${ASM_SRC}.map > build/${ASM_SRC}.symtab
+avr-objdump -d ${BUILD_DIR}/${ASM_FILE}.elf| python2 tools/extract-symbols-metadata.py ${BUILD_DIR}/${ASM_FILE}.map > ${BUILD_DIR}/${ASM_FILE}.symtab
 
 if [ ${RELOC} -eq 1 ]; then
-  cat build/${ASM_SRC}.symtab | tools/elf-add-symbol build/${ASM_SRC}.elf
+  cat ${BUILD_DIR}/${ASM_FILE}.symtab | tools/elf-add-symbol ${BUILD_DIR}/${ASM_FILE}.elf
 fi
-avr-gcc -mmcu=atmega328p -Os -DF_CPU=16000000 -DASM_SYM=${SYMBOL_NAME} -o build/main_${ASM_SRC}.elf main.c build/${ASM_SRC}.elf
-avr-objcopy -I elf32-avr -O ihex -j .text -j .data build/main_${ASM_SRC}.elf build/main_${ASM_SRC}.hex
+avr-gcc -mmcu=${AVR_CHIP} -Os -DF_CPU=1000000 -DASM_SYM=${SYMBOL_NAME} -o ${BUILD_DIR}/main_${ASM_FILE}.elf main.c ${BUILD_DIR}/${ASM_FILE}.elf
+avr-objcopy -I elf32-avr -O ihex -j .text -j .data ${BUILD_DIR}/main_${ASM_FILE}.elf ${BUILD_DIR}/main_${ASM_FILE}.hex
 
-sudo /usr/share/arduino/hardware/tools/avrdude -C /usr/share/arduino/hardware/tools/avrdude.conf -patmega328p -cusbasp -Uflash:w:build/main_${ASM_SRC}.hex:i
+sudo /usr/share/arduino/hardware/tools/avrdude -C /usr/share/arduino/hardware/tools/avrdude.conf -p${AVR_CHIP} -cusbasp -Uflash:w:${BUILD_DIR}/main_${ASM_FILE}.hex:i
 
